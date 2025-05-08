5df2da7d2d7508054a7640d37d3b8c452344a021
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-05-08 11:43:16 +0300

    Blazor Fundamentals Part 1

---
+++ LearningBlazor/Components/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace LearningBlazor.Components.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int QuantityInStock { get; set; }
+        public bool IsAvailable { get; set; }
+        public IEnumerable<ProductPropriety> productProprieties { get; set; }
+    }
+    public class ProductPropriety
+    {
+        public string Name { get; set; } 
+        public string Value { get; set; }
+    }
+}
